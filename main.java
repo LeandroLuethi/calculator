9e4aa5c65cb33dd94c6b3bf2aaa8810b19068232
Pascal Zürcher <90255653+pascalzuercher@users.noreply.github.com>
2022-01-17 14:16:29 +0100

    mnn

--- main.java
+++ main.java
@@ -62,7 +62,6 @@ public class main {
        jPanel1.add(buttonnine);
        jPanel1.setLayout( new java.awt.GridLayout( 4, 5 ) );
        frame.add(jPanel1);$
-       khdjfgkgrghbtkbhhhuuhub
-           hallo
+       
     }
 }
